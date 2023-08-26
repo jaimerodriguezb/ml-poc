b949637e391ad61d2022415d8128825bdcef11c9
Jaime Rodriguez <jaimerodriguez@gmail.com>
2023-08-26 11:29:52 -0500

    RED - and, or test, improving fixture

--- tests/test_gates.py
+++ tests/test_gates.py
@@ -4,14 +4,40 @@ def logic_gate(a, b, operator):
     return True
 
 @pytest.fixture
-def get_true_input():
+def true_input():
     return 1
 
 @pytest.fixture
-def get_false_input():
+def false_input():
     return 0
 
-def test_and_ok(get_true_input):
-    result = logic_gate(get_true_input, get_true_input, 'and')
+@pytest.fixture
+def and_gate():
+    return 'and'
+
+@pytest.fixture
+def or_gate():
+    return 'or'
+
+@pytest.fixture
+def nor_gate():
+    return 'nor'
+
+@pytest.fixture
+def xor_gate():
+    return 'xor'
+
+@pytest.fixture
+def nand_gate():
+    return 'nand'
+
+
+def test_and_true(true_input, and_gate):
+    result = logic_gate(true_input, true_input, and_gate)
+
+    assert result
+
+def test_or_true(true_input,false_input):
+    result = logic_gate(true_input, false_input, or_gate)
 
     assert result
